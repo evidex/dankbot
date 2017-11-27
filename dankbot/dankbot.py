8ab9155efbc88e93e1d553aece5b4a50bbb1ba3b
David Hayes <evidex@freeshell.de>
2017-11-27 17:41:48 +0000

    Add some initial loop

--- dankbot/dankbot.py
+++ dankbot/dankbot.py
@@ -2,6 +2,22 @@
 
 from utils import *
 
+import argparse
+
 class Dankbot:
     def __init__(self, verbose=True, debug=False):
         self.logger = setup_logger(self.__name__, verbose, debug)
+
+    def run(self):
+        done = False
+        while not done:
+            km = self.kb.fetch_killmail()
+            for endpoint in self.endpoints:
+                endpoint.post_km(km)
+        # Tidy up exit out here
+
+if __name__ == '__main__':
+    # Setup parser
+
+    pass
+
